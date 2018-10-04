1abacf1e550d9f0905c01f0ffb50084655303f75
vvselischev <800-99@mail.ru>
2018-10-04 23:01:28 +0300

    Update Parser.hs

--- Parser.hs
+++ Parser.hs
@@ -5,9 +5,11 @@ import Prelude hiding (lookup)
 
 data AST = ASum Operator AST AST
          | AProd Operator AST AST
-         | AAssign Char AST
+         | AUnaryMinus AST
+         | APow Operator AST AST
+         | AAssign String AST
          | ANum Integer
-         | AIdent Char
+         | AIdent String
 
 parse :: String -> Maybe AST
 parse input =
@@ -35,12 +37,29 @@ expression ts =
 
 term :: [Token] -> (AST, [Token])
 term ts =
-  let (factNode, ts') = factor ts in
+  let (powNode, ts') = powTerm ts in
   case lookup ts' of
     TOp op | op == Mult || op == Div ->
       let (termNode, ts'') = term $ accept ts' in
-      (AProd op factNode termNode, ts'')
-    _ -> (factNode, ts')
+      (AProd op powNode termNode, ts'')
+    _ -> (powNode, ts')
+    
+powTerm :: [Token] -> (AST, [Token])
+powTerm ts =
+  let (unaryNode, ts') = unaryTerm ts in
+  case lookup ts' of
+    TOp op | op == Pow ->
+      let (termNode, ts'') = powTerm $ accept ts' in
+      (APow op unaryNode termNode, ts'')
+    _ -> (unaryNode, ts')
+    
+unaryTerm :: [Token] -> (AST, [Token])
+unaryTerm ts =
+  case lookup ts of
+    TOp op | op == Minus ->
+      let (termNode, ts'') = unaryTerm $ accept ts in
+      (AUnaryMinus termNode, ts'')
+    _ -> factor ts
 
 factor :: [Token] -> (AST, [Token])
 factor ts =
@@ -51,8 +70,10 @@ factor ts =
         TRParen -> (exprNode, accept ts')
         _ -> error "Syntax error: mismatched parentheses"
     TIdent v -> (AIdent v, accept ts)
-    TDigit d -> (ANum d, accept ts)
+    TNumber d -> (ANum d, accept ts)
     _ -> error "Syntax error: factor can only be a digit, an identifier or a parenthesised expression"
+
+
 
 lookup :: [Token] -> Token
 lookup = head
@@ -68,7 +89,9 @@ instance Show AST where
         (case t of
                   ASum  op l r -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
                   AProd op l r -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
-                  AAssign  v e -> v : " =\n" ++ show' (ident n) e
+                  AAssign  v e -> v ++ " =\n" ++ show' (ident n) e
+                  AUnaryMinus v -> "-\n" ++ show' (ident n) v
+                  APow op l r -> showOp op : "\n" ++ show' (ident n) l ++ "\n" ++ show' (ident n) r
                   ANum   i     -> show i
                   AIdent i     -> show i)
       ident = (+1)
@@ -76,3 +99,4 @@ instance Show AST where
       showOp Minus = '-'
       showOp Mult  = '*'
       showOp Div   = '/'
+      showOp Pow   = '^'
